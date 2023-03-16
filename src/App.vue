d4d81b24a23ed4271c46b7804d959dd4dbd40e1b
CuiYanling <cyl_2379@126.com>
2023-03-16 20:17:14 +0800

    第一次提交

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+<side-bar></side-bar>
+</template>
+
+<script>
+import SideBar from './components/SideBar.vue'
+
+export default {
+  name: 'App',
+  components: {
+    SideBar
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/SideBar.vue
@@ -0,0 +1,21 @@
+<template>
+  <a-layout>
+    <a-layout-sider :trigger="null" collapsible>
+      <a-menu theme="dark" mode="inline">
+        <a-menu-item key="1">
+          <span>折线图</span>
+        </a-menu-item>
+        <a-menu-item key="2">
+          <span>饼状图</span>
+        </a-menu-item>
+        <a-menu-item key="3">
+          <span>柱状图</span>
+        </a-menu-item>
+      </a-menu>
+    </a-layout-sider>
+  </a-layout>
+</template>
+
+<script>
+
+</script>
